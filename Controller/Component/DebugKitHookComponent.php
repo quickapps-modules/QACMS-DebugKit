623088f130c9d1b3e9254efc1dcae322334b665e
QuickApps <info@quickapps.es>
2011-11-01 17:57:49 +0100

    Fix debug mode should be forced to level 2 when module is active

--- Controller/Component/DebugKitHookComponent.php
+++ Controller/Component/DebugKitHookComponent.php
@@ -4,6 +4,7 @@ class DebugKitHookComponent extends Component {
         if ($Controller->Quickapps->isAdmin() ||
             count(array_intersect((array)Configure::read('Modules.DebugKit.settings.roles'), $Controller->Quickapps->userRoles()))
         ) {
+            Configure::write('debug', 2);
             $Controller->Toolbar = $Controller->Components->load('DebugKit.Toolbar', array('enabled' => true));
             $Controller->Toolbar->startup($Controller);
             $Controller->Toolbar->initialize($Controller);
